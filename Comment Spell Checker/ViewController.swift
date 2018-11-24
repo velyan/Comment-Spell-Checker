455bd0765131fa8b3fbae121287c4e356201e105
Velislava Yanchina <vel.yanchina@yahoo.com>
2018-11-24 20:05:46 +1100

    Initial Commit

---
+++ Comment Spell Checker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Comment Spell Checker
+//
+//  Created by Velislava Yanchina on 24/11/18.
+//  Copyright © 2018 Velislava Yanchina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Comment Spell CheckerTests/Comment_Spell_CheckerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Comment_Spell_CheckerTests.swift
+//  Comment Spell CheckerTests
+//
+//  Created by Velislava Yanchina on 24/11/18.
+//  Copyright © 2018 Velislava Yanchina. All rights reserved.
+//
+
+import XCTest
+@testable import Comment_Spell_Checker
+
+class Comment_Spell_CheckerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
